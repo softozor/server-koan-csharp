ffc857140ee729d3200a6da300fecffd410a70d1
Laurent Michel <laurent.michel@softozor.ch>
2019-03-01 15:03:31 +0100

    Provide minimalistic server
    * build and run default web server
    * provide minimal Startup class
    * add initial editorconfig (got from msdn examples)
    * add fxcop config

--- ServerKoan/Program.cs
+++ ServerKoan/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ServerKoan
 {
@@ -6,7 +8,13 @@ namespace ServerKoan
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      BuildWebHost(args).Run();
     }
+
+    private static IWebHost BuildWebHost(string[] args) =>
+      WebHost.CreateDefaultBuilder(args)
+       .UseStartup<Startup>()
+       .UseUrls("http://localhost:8000")
+       .Build();
   }
 }
---
+++ ServerKoan/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServerKoan
+{
+  class Startup : StartupBase
+  {
+    //public override void ConfigureServices(IServiceCollection services)
+    //{
+
+    //}
+
+    public override void Configure(IApplicationBuilder app)
+    {
+
+    }
+  }
+}
