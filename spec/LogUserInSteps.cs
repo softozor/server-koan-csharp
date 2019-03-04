d4df05af7832586b9f50cc00180ce482cf0dbb06
Laurent Michel <laurent.michel@softozor.ch>
2019-03-04 15:00:23 +0100

    Add TestServer
    * install necessary packages for entity framework
    * use config and hosting environment in startup class
    * configure the spec app so that debugging is allowed during tests
    * add Hooks class
    * inject static TestServer in LogUserInSteps
    * use static class starting a test server

--- spec/LogUserInSteps.cs
+++ spec/LogUserInSteps.cs
@@ -1,51 +1,54 @@
-﻿using System;
+﻿using System.Net.Http;
 using TechTalk.SpecFlow;
 
 namespace spec
 {
-    [Binding]
-    public class LogUserInSteps
+
+  [Binding]
+  public class LogUserInSteps
+  {
+    private readonly HttpClient client = TestServer.Client;
+
+    [Given(@"the User is not logged in")]
+    public void GivenTheUserIsNotLoggedIn()
     {
-        [Given(@"the User is not logged in")]
-        public void GivenTheUserIsNotLoggedIn()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [When(@"the User registers with valid username and password")]
-        public void WhenTheUserRegistersWithValidUsernameAndPassword()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [When(@"a User registers with invalid username and password")]
-        public void WhenAUserRegistersWithInvalidUsernameAndPassword()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [When(@"a User registers with valid username and invalid password")]
-        public void WhenAUserRegistersWithValidUsernameAndInvalidPassword()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"his session opens for (.*) week")]
-        public void ThenHisSessionOpensForWeek(int p0)
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"he gets a wrong credentials response")]
-        public void ThenHeGetsAWrongCredentialsResponse()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"he gets a wrong credententials response")]
-        public void ThenHeGetsAWrongCredententialsResponse()
-        {
-            ScenarioContext.Current.Pending();
-        }
+      ScenarioContext.Current.Pending();
     }
+
+    [When(@"the User registers with valid username and password")]
+    public void WhenTheUserRegistersWithValidUsernameAndPassword()
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [When(@"a User registers with invalid username and password")]
+    public void WhenAUserRegistersWithInvalidUsernameAndPassword()
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [When(@"a User registers with valid username and invalid password")]
+    public void WhenAUserRegistersWithValidUsernameAndInvalidPassword()
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [Then(@"his session opens for (.*) week")]
+    public void ThenHisSessionOpensForWeek(int p0)
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [Then(@"he gets a wrong credentials response")]
+    public void ThenHeGetsAWrongCredentialsResponse()
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [Then(@"he gets a wrong credententials response")]
+    public void ThenHeGetsAWrongCredententialsResponse()
+    {
+      ScenarioContext.Current.Pending();
+    }
+  }
 }
---
+++ spec/TestServer.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using Api;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace spec
+{
+  public static class TestServer
+  {
+    private static HttpClient client;
+
+    private static IWebHostBuilder GetWebHostBuilder() =>
+     WebHost.CreateDefaultBuilder()
+      .UseEnvironment("Test")
+      .UseStartup<Startup>();
+
+    static public HttpClient Client
+    {
+      get
+      {
+        if (client == null)
+        {
+          var builder = GetWebHostBuilder();
+          var server = new Microsoft.AspNetCore.TestHost.TestServer(builder);
+          client = server.CreateClient();
+        }
+        return client;
+      }
+    }
+  }
+}
